265d45ebbcd8bf98012f01bc025750a0d3a1afd9
dougladmo <Douglas.costa7417@gmail.com>
2024-05-10 16:27:33 -0300

    cursor pointer adicionado na newslettter do footer

--- src/components/Layout/Footer/NewsLetter.tsx
+++ src/components/Layout/Footer/NewsLetter.tsx
@@ -7,7 +7,7 @@ const NewsLetter = () => {
             <form className='grid grid-cols-3 grid-rows-2 gap-2'>
                 <input className="col-span-3 indent-2 py-2 border-[1px] border-white bg-transparent rounded-lg" type="email" placeholder='Digite seu e-mail' id='email' name='email' required />
                 <input className="col-span-2 indent-2 py-2 border-[1px] border-white bg-transparent rounded-lg" type="name" placeholder='Digite seu nome' id='name' name='name' required />
-                <input className="col-span-1 bg-white text-blue-800 py-1 rounded-lg hover:text-white hover:bg-transparent border-2 border-white transition duration-500" type="submit" id='submit' name='submit' />
+                <input className="cursor-pointer col-span-1 bg-white text-blue-800 py-1 rounded-lg hover:text-white hover:bg-transparent border-2 border-white transition duration-500" type="submit" id='submit' name='submit' />
             </form>
         </div>
     )
