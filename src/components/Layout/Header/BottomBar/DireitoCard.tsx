15354c0203e42751f071aaf9a0dcb70c9655e761
Dougladmo <douglas.costa7417@gmail.com>
2024-05-17 14:42:34 -0300

    bottom header animação corrigida

--- src/components/Layout/Header/BottomBar/DireitoCard.tsx
+++ src/components/Layout/Header/BottomBar/DireitoCard.tsx
@@ -25,7 +25,7 @@ const DireitoCard = ({ title, img, rounded }: CardProps) => {
                 className="object-cover w-full h-full"
             />
             <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-                <div className={`flex flex-col items-center justify-center gap-2 ${isOpen ? 'wrapper-opened md:-translate-y-8' : 'wrapper-closed -translate-y-0'} `}>
+                <div className={`flex flex-col items-center justify-center gap-2 ${isOpen ? 'wrapper-opened md:-translate-y-6' : 'wrapper-closed -translate-y-0'} `}>
                     <div className="flex items-center justify-center cursor-pointer">
                         <div className="h-[1px] bg-white w-5 md:w-7 mr-3"></div>
                         <p className="uppercase font-sans text-base font-medium">Direito</p>
@@ -33,7 +33,11 @@ const DireitoCard = ({ title, img, rounded }: CardProps) => {
                     </div>
                     <h2 className="uppercase font-bold text-2xl md:text-3xl text-center cursor-pointer">{title}</h2>
                 </div>
-                {isOpen && <Link href='#' className={`${isOpen ? 'button-opened' : null} hidden md:flex bg-transparent text-white rounded-lg py-4 px-6 transition hover:bg-white hover:text-black duration-500 border-[1px] border-white`}>Saiba mais</Link>}
+                {isOpen ? 
+                    <Link href='#' className={`button-opened hidden md:flex bg-transparent text-white rounded-lg py-4 px-6 transition hover:bg-white hover:text-black duration-500 border-[1px] border-white`}>Saiba mais</Link>
+                    :
+                    <Link href='#' className={`button-closed hidden bg-transparent text-white rounded-lg py-4 px-6 transition hover:bg-white hover:text-black duration-500 border-[1px] border-white`}>Saiba mais</Link>
+                }
             </div>
         </div>
     )
