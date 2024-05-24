98885b9e33489617491e3e886b3f9511f4da5df3
Dougladmo <douglas.costa7417@gmail.com>
2024-05-24 16:50:34 -0300

    areas de atuacao finalizadas

--- src/components/AreasAtuacao/CardLinksAreas.tsx
+++ src/components/AreasAtuacao/CardLinksAreas.tsx
@@ -5,7 +5,7 @@ const CardLinks = () => {
   return (
     <div className='bg-white max-h-[340px] pb-12 hidden lg:flex'>
       <div className='bg-gray-300 p-5 py-7 rounded-lg w-64 flex flex-col gap-3'>
-        <h4 className='text-blue-800 uppercase text-xl font-bold'>institucional</h4>
+        <h4 className='text-blue-800 uppercase text-xl font-bold'>áreas de atuação</h4>
         <CustomLink link='/areas-atuacao/direito-administrativo/' text='Direito Administrativo' />
         <CustomLink link='/areas-atuacao/direito-tributario/' text='Direito Tributário' />
         <CustomLink link='/areas-atuacao/direito-civil/' text='Direito Civil' />
